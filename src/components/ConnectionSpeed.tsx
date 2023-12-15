99f7524603f18d5d0adc7de3fea2b1d9af7a74e1
Jaseel <jaseelta1@gmail.com>
2023-12-15 22:46:09 +0530

    fixed build error

--- src/components/ConnectionSpeed.tsx
+++ src/components/ConnectionSpeed.tsx
@@ -1,44 +1,12 @@
-import  { useEffect, useState } from "react";
+import  { useState } from "react";
 import "../../node_modules/react-internet-meter/dist/index.css";
 import { ReactInternetSpeedMeter } from "react-internet-meter";
 
-import { ILocationData } from "../interface/ILocatiionData";
-import axios from "axios";
 function ConnectionSpeed() {
 
-  
   const [checkSpeed, SetCheckSpeed] = useState(0);
-  const [location, setLocation] = useState<ILocationData|null>(null);
-  const VITE_IP_STACK_ACCESS_KEY = import.meta.env.VITE_IP_STACK_ACCESS_KEY as string
-  
-    const getIP = async () => {
-        const res = await axios.get("https://api.ipify.org/?format=json");
-    
-      return res.data.ip
-       
-    };
-  
-  
-  const fetchNetworkDetails = async (ip: number) => {
-    const result = await axios.get(`http://api.ipstack.com/${ip}?access_key=${VITE_IP_STACK_ACCESS_KEY}`)
-    setLocation(result.data)
-  }
  
-
   
-
-  useEffect(() => {
-   
-    (async function bundle() {
-      
-      
-      fetchNetworkDetails(await getIP())
-    })()
-
-
-  }, []);
-
-   
   return (
 <>
       <ReactInternetSpeedMeter
