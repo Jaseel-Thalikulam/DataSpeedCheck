7635ed8337baf7827b7dc145fc8a1871534da387
Jaseel <jaseelta1@gmail.com>
2023-12-15 23:40:29 +0530

    added logo and changed title

--- src/components/ConnectionSpeed.tsx
+++ src/components/ConnectionSpeed.tsx
@@ -1,7 +1,7 @@
 import  { useState } from "react";
 import "../../node_modules/react-internet-meter/dist/index.css";
 import { ReactInternetSpeedMeter } from "react-internet-meter";
-
+import LOGO from '../assets/SpeedWay (1).png'
 function ConnectionSpeed() {
 
   const [checkSpeed, SetCheckSpeed] = useState(0);
@@ -26,18 +26,30 @@ function ConnectionSpeed() {
         callbackFunctionOnNetworkTest={(data:number) => SetCheckSpeed(data)}
       />
       
+      <div className="h-screen flex flex-col">
 
-      
-      <div className="h-screen flex justify-center items-center select-none insp">
-  <div className=" lg:flex lg:space-x-80 "> 
-    <div className="md: mb-10 text-violet-700"> 
-      <h1 className="font-black text-9xl">{checkSpeed} <span className="text-5xl">MB/s</span></h1>
+<div className="flex-none">
+  {/* Add your logo image or content here */}
+  <img src={LOGO} alt="Logo" className="h-40 w-40" />
+</div>
+
+<div className="flex-grow flex justify-center items-center ">
+ 
+
+  <div className="h-auto flex justify-center items-center select-none insp pb-16">
+    <div className="md:mb-10 text-violet-700"> 
+      <h1 className="font-black text-4xl md:text-6xl lg:text-7xl xl:text-8xl 2xl:text-10xl">
+        {checkSpeed} <span className="lg:text-4xl md:text-xl xl:text-2xl 2xl:text-5xl">MB/s</span>
+      </h1>
     </div>
-
-
-
   </div>
 </div>
+</div>
+
+
+
+
+
 
       </>
   );
